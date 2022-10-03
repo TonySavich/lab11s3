bee1cea016e513a6d798b7d4242a9502aa942a44
TonySavich <92541859+TonySavich@users.noreply.github.com>
2022-10-03 20:33:46 +0200

    Add files via upload

---
+++ lab11.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+int const n = 10;
+
+
+void quik (int *mas, int size) {
+    int i = 0;
+    int j = size - 1;
+    int sr = mas[size / 2];
+    do {
+        while (mas[i] < sr) {
+            i++;
+        }
+        while (mas[j] > sr) {
+            j--;
+        }
+        if (i <= j) {
+            std::swap(mas[i], mas[j]);
+            i++;
+            j--;
+        }
+    } while (i <= j);
+    if (j > 0) {   
+        quik(mas, j + 1);
+    }
+    if (i < size) {
+        quik(&mas[i], size - i);
+    }
+}
+
+
+
+
+int main()
+{
+    int mas[n];
+    for (int i = 0; i < n; i++) {
+        mas[i] = rand() % 11;
+    }
+    for (int i = 0; i < n; i++) {
+        std::cout << mas[i] << " ";
+    }
+    std::cout << std::endl;
+
+    quik(mas, n);
+
+    for (int i = 0; i < n; i++) {
+        std::cout << mas[i] << " ";
+    }
+}
+
